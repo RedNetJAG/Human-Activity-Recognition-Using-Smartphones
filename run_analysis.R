c506f8836e6331d257d598b522bbbe66ba733339
Jose <rednet@openmailbox.org>
2014-07-24 13:08:22 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,12 +2,15 @@
 
 library(reshape2)
 
-# Read data from files
-features = read.table('./Dataset/features.txt')
-activityLabels = read.table('./Dataset/activity_labels.txt')
-subjectTrain = read.table('./Dataset/train/subject_train.txt')
-xTrain = read.table('./Dataset/train/x_train.txt')
-yTrain = read.table('./Dataset/train/y_train.txt')
-subjectTest = read.table('./Dataset/test/subject_test.txt')
-xTest = read.table('./Dataset/test/x_test.txt')
-yTest = read.table('./Dataset/test/y_test.txt')
+## Read data from files
+features = read.table("./Dataset/features.txt")
+activityLabels = read.table("./Dataset/activity_labels.txt")
+subjectTrain = read.table("./Dataset/train/subject_train.txt")
+xTrain = read.table("./Dataset/train/x_train.txt")
+yTrain = read.table("./Dataset/train/y_train.txt")
+subjectTest = read.table("./Dataset/test/subject_test.txt")
+xTest = read.table("./Dataset/test/x_test.txt")
+yTest = read.table("./Dataset/test/y_test.txt")
+
+## Labels columns
+colnames(activityLabels) <- c("activityId", "activityType")
