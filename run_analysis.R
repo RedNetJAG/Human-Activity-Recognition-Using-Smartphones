9b4baa7122144239de12c8a40c64a98c6f67819e
Jose <rednet@openmailbox.org>
2014-07-24 13:53:17 +0200

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,28 @@
+
+
+library(reshape2)
+
+## Read data from files
+features = read.table("./Dataset/features.txt")
+activityLabels = read.table("./Dataset/activity_labels.txt")
+subjectTrain = read.table("./Dataset/train/subject_train.txt")
+xTrain = read.table("./Dataset/train/x_train.txt")
+yTrain = read.table("./Dataset/train/y_train.txt")
+subjectTest = read.table("./Dataset/test/subject_test.txt")
+xTest = read.table("./Dataset/test/x_test.txt")
+yTest = read.table("./Dataset/test/y_test.txt")
+
+## Labels the columns
+colnames(activityLabels) <- c("activityId", "activityType")
+colnames(subjectTrain) <- "subjectId"
+colnames(subjectTest) <- "subjectId"
+colnames(yTrain) <- "activityId"
+colnames(yTest) <- "activityId"
+colnames(xTrain) <- features$V2
+colnames(xTest) <- features$V2
+
+## Merges de trainig dataset and the test dataset
+Training <- cbind(subjectTrain, xTrain, yTrain)
+Test <- cbind(subjectTest, xTest, yTest)
+## Result for 1.Merges the training and the test sets to create one dataset
+TrainingTest <- rbind(Training, Test)
